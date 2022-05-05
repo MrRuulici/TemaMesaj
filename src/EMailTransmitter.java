7a7c86621faf07f339638cbb55182d0e457e5040
MrRuulici <92568296+MrRuulici@users.noreply.github.com>
2022-05-05 19:02:33 +0300

    Add files via upload

---
+++ src/EMailTransmitter.java
@@ -0,0 +1,23 @@
+public class EMailTransmitter implements Transmitter {
+
+	public static final String transmitterTypeName = "EMAIL";
+	private Message message = null;
+
+	public void store(Message message) {
+		this.message = message;
+		Person receiver = message.getReceiver();
+		receiver.notify(this);
+
+	}
+
+	public Message retrieve(Person receiver) {
+		if (receiver.equals(message.getReceiver()))
+			return message;
+		return null;
+	}
+
+	public String transmitterTypeName() {
+		return transmitterTypeName;
+	}
+
+}
